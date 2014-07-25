b83fd14726d9e0fe22ed32ffc0e7b44f138d3c24
Meghan Lindsley <student@epicodus.com>
2014-07-25 14:31:31 -0700

    Adds and passes a second spec for calculating a multiple letter roman numeral.

--- lib/reverse_numerals.rb
+++ lib/reverse_numerals.rb
@@ -1,13 +1,13 @@
 def reverse_numerals(letter)
   letter = letter.split("")
   numeral_hash = {"I" => 1, "V"=>5, "X"=>10, "L"=>50, "C"=>100, "D" => 500, "M"=>1000 }
-  results = []
+  results = 0
 
   letter.each do |element|
-    results << numeral_hash[element]
+    results += numeral_hash[element]
   end
 
-  puts results
+  results
 end
 
-reverse_numerals("I")
+
--- spec/reverse_numerals_spec.rb
+++ spec/reverse_numerals_spec.rb
@@ -2,7 +2,11 @@ require("rspec")
 require("reverse_numerals")
 
 describe("reverse_numerals") do
-  it("returns one number when given a single roman numeral")
-    expect(reverse_numerals("I")).to(eq(1))
+  it("returns one number when given a single roman numeral") do
+    reverse_numerals("I").should(eq(1))
+  end
+
+  it("returns a multiple digit number when given multiple numerals") do
+    reverse_numerals("XV").should(eq(15))
   end
 end
